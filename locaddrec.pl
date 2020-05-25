e8074574dbc8701b5a5f18218e21397aa27e090e
Bluespics <tony.winfield@ntlworld.com>
2020-05-25 22:12:29 +0100

    25052020

--- locaddrec.pl
+++ locaddrec.pl
@@ -3,11 +3,13 @@ use warnings;
 use DBI;
 use Time::Local;
 
+# Initialise variables
 $savelatitude = '0';
 $savelongitude = '0';
 $saveepoc = '0';
 $recordno = 1;
 
+# Open database
 $driver = "SQLite";
 $database = "photoloc.db";
 $dsn = "DBI:$driver:dbname=$database";
@@ -17,6 +19,7 @@ $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 }) or die $DBI::
 
 print "Opened database successfully\n";
 
+# Open file
 open FH, '<', 'split1.txt' or die "Cannot open file $!";
 
 while(my $line = <FH>){
@@ -41,27 +44,34 @@ while(my $line = <FH>){
     $min = $tstr[1];
     $sec = $tstr[2];
 
+    # Short version of latitude and longitude to allow tolerance in matches
     $lattest = sprintf("%.3f", $fields[3]);
     $lontest = sprintf("%.3f", $fields[4]);
 
-    next if(($lattest == 52.978) && ($lontest == -1.199));
-    next if(($lattest == 52.766) && ($lontest == -0.869));
-    next if(($lattest == 52.977) && ($lontest == -1.298));
+    # Exclude home, work and other places
+    next if($lattest == 52.978 and $lontest == -1.199);
+    next if($lattest == 52.977 and $lontest == -1.199);
+    next if($lattest == 52.766 and $lontest == -0.869);
+    next if($lattest == 52.977 and $lontest == -1.298);
 
     # Round latitude and longitude to 6 decimal places
     $longitude = sprintf("%.6f", $fields[2]);
     $latitude = sprintf("%.6f", $fields[3]);
 
+    # Consolidate same locations
     next if($latitude == $savelatitude and $longitude == $savelongitude);
 
     # Get Epoch time
     $epoc = timelocal($sec, $min, $hours, $mday, $mon, $year);
 
+    # Write to database
     if($recordno > 1){
         $stmt = qq(INSERT INTO LOCATION (ID, STARTSECONDS, ENDSECONDS, LATITUDE, LONGITUDE)
                         VALUES (NULL, $saveepoc, $epoc, $savelatitude, $savelongitude));
         $dbh->do($stmt);
     }
+
+    # Overwrite saved values for next match test
 
     $recordno += 1;
     $savelatitude = $latitude;
@@ -70,6 +80,7 @@ while(my $line = <FH>){
     print("$recordno\n");
 }
 
+# Close down file and database and inform user
 close(FH);
 print "Records created successfully\n";
 $dbh->disconnect();
