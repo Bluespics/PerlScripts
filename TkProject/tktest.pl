7169d19b220d489b219a0f1d4fe9fa5ca0c0daf0
Bluespics <tony.winfield@ntlworld.com>
2020-06-15 21:17:08 +0100

    TKpractice150620

--- TkProject/tktest.pl
+++ TkProject/tktest.pl
@@ -1,13 +1,61 @@
 
-# Display Hello World program
-
-use warnings;
-use strict;
 use Tk;
+use strict;
 
-my $mw = MainWindow->new( -width => '400', -relief => 'flat', -height => '100', -title => 'Exif Updater', );
-my $var1 = $mw->Entry( -width => '50', -relief => 'sunken', -state => 'normal', -justify => 'left', -text => 'Entry0', )->place( -x => 63, -y => 11);
-my $var2 = $mw->Label( -pady => '1', -relief => 'flat', -padx => '1', -state => 'normal', -justify => 'center', -text => 'File Path', )->place( -x => 10, -y => 10);
-my $var3 = $mw->Button( -pady => '1', -relief => 'raised', -padx => '1', -state => 'normal', -justify => 'center', -text => 'Run Program', )->place( -x => 18, -y => 60);
+# initial banner text. Entry is not read-only
+my $str = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
 
-$mw->MainLoop();
+my $mw = MainWindow->new;
+my $lframe = $mw->Frame->pack(-fill => 'both',
+    -side => 'left', -expand => 1);
+my $lb = $lframe->Scrolled("Listbox", -scrollbars => "e",
+    -height => 3)->pack(-fill => 'both', -expand => 1, -side => 'top');
+
+$lb->insert('end', sort $mw->fontFamilies);
+
+# Button that will pop the config widgets in and out
+my $hidebutton = $mw->Button(-text => ">")->pack(-side => 'left',
+    -fill => 'y');
+$hidebutton->configure(-command =>
+    sub {
+        if ($hidebutton->cget(-text) eq ">") {
+             $lframe->packForget; $hidebutton->configure(-text => "<")
+         } else {
+              $lframe->pack(-before => $hidebutton, -fill => 'both',
+                -side => 'left', -expand => 1);
+                $hidebutton->configure(-text => ">");
+            }
+    }, -font => "courier 8");
+
+    my $entry = $mw->Entry(
+                    -textvariable => \$str,
+                    -width => 12,
+                    -font => "{Comic Sans MS} 72",
+                    -relief => 'raised',
+                    -highlightthickness => 0,
+                    )->pack(-expand => 1, -fill => 'x', -side => 'left');
+
+    $lb->bind("", sub { $entry->configure(
+        -font => "{". $lb->get($lb->curselection) . "} 72"); });
+
+    my $repeat_id = $mw->repeat(300, \&shift_banner);
+
+    my $f = $lframe->Frame->pack(-side => 'bottom', -fill => 'y');
+    my $start_button;
+    $start_button = $f->Button(-text => "Start",
+        -command => sub {
+            $repeat_id = $mw->repeat(300,\&shift_banner);
+            $start_button->configure(-state => 'disabled'); },
+            -state => 'disabled')->pack(-side => 'left', -padx => 3);
+    my $stop_button = $f->Button(-text => "Stop", -command => sub {
+        $repeat_id->cancel( );
+        $start_button->configure(-state => 'normal'); }
+    )->pack(-side => 'left', -padx => 3);
+
+MainLoop;
+
+# Causes text to be wrapped around in entry
+sub shift_banner {
+    my $newstr = substr($str, 1) . substr($str, 0, 1);
+    $str = $newstr;
+}
