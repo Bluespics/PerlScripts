800b2393b87d67b2bbd6d4262f8a9366d8fcc062
Bluespics <tony.winfield@ntlworld.com>
2020-07-06 20:49:41 +0100

    06072020

--- getexif.pl
+++ getexif.pl
@@ -25,9 +25,6 @@ my $frm_name = $mw -> Frame();
 my $lab = $frm_name -> Label(-text => "File Path: ");
 my $ent = $frm_name -> Entry(-width => 110);
 
-my $but = $mw -> Button(-text => "Get EXIF", -command => \&push_button);
-my $but2 = $mw -> Button(-text => "Clear All", -command => \&clear_screen);
-
 my $textarea = $mw -> Frame();   # Creating another Frame
 my $txt = $textarea -> Text(-width => 120, -height => 50);
 my $srl_y = $textarea -> Scrollbar(-orient => 'v', -command => [yview => $txt]);
@@ -43,9 +40,6 @@ $lab -> grid(-row => 1, -column => 1);
 $ent -> grid(-row => 1, -column => 2, -columnspan => 3);
 $frm_name -> grid(-row => 1, -column => 1, -columnspan => 4);
 
-$but -> grid(-row => 4, -column => 1, -sticky => 'e');
-$but2 -> grid(-row => 4, -column => 2, -sticky => 'w');
-
 $txt -> grid(-row => 1, -column => 1);
 $srl_y -> grid(-row => 1, -column => 2, -sticky => "ns");
 $srl_x -> grid(-row => 2, -column => 1, -sticky => "ew");
@@ -54,17 +48,16 @@ $textarea -> grid(-row => 5, -column => 1, -columnspan => 2,
 
 MainLoop;
 
-sub open_file {
-  my $open = $mw->getOpenFile(-filetypes => $types,
-                            -defaultextension => '.jpg');
-  $ent -> insert('end', "$open") if $open;
-}
-
 sub clear_screen {
     $txt -> delete('1.0', 'end');
     $ent -> delete('0.0', 'end');
 }
-sub push_button {
+
+sub open_file {
+    my $open = $mw->getOpenFile(-filetypes => $types,
+                            -defaultextension => '.jpg');
+    $ent -> insert('end', "$open") if $open;
+
     my $fdir = $ent -> get();
 
     my $exifTool = new Image::ExifTool;
